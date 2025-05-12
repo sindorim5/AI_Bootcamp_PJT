b2f02055ee6afa32ce767805489f5968729dc2a5
Kihun SONG <kihunbuzz@naver.com>
2025-05-13 00:08:18 +0900

    init 5_final

---
+++ 5_final/finance_app/main.py
@@ -0,0 +1,13 @@
+
+def init_session_state():
+    return None
+
+def render_ui():
+    return None
+
+if __name__ == "__main__":
+    init_session_state()
+
+    # db_session.initialize()
+
+    render_ui()
